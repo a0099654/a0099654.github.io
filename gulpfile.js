1fb18ede1a9874f6066cc57d082b257ac00626eb
a0099654 <a0099654@u.nus.edu>
2016-02-20 16:06:39 +0800

    adding minify js

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,12 @@
 var gulp = require('gulp');
 var uglify = require('gulp-uglify');
 
-gulp.task('default', function(){
+gulp.task('minifyjs', function(){
     gulp.src('js/*.js')
         .pipe(uglify())
-        .pipe(gulp.dest('minjs'))
+        .pipe(gulp.dest('minjs'));    
+});
+
+gulp.task('default', function(){
+    
 });
